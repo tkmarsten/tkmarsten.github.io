619335a53ecb776dbff5ca1fe68b54bf0cdbf7cc
tiarnanm <tkmarsten@gmail.com>
2023-04-12 17:23:55 -0700

    Test change for Vercel deployment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <section className="banner">
       <img src="img/profile.jpg" alt="profile pic" className="profile-pic" />
       <div>
-        <h1>Hi, I'm <span>Tiarnan</span></h1>
+        <h1>Hi, I'm <span>Tiarnan Marsten</span></h1>
         <h2>I'm a software and full stack web developer that enjoys building and designing web applications.</h2>
         <a href="#about"><span>Learn more about me</span></a>
       </div>
